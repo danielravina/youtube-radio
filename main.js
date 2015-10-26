7b24a47719a2d6af9b3a1d29c76eff12be9e0696
DanielRavina <danielravina@gmail.com>
2015-10-26 09:06:02 -0700

    first commit -u origin master

---
+++ main.js
@@ -0,0 +1,42 @@
+(function() {
+  var $videoContainer = $(".html5-video-container");
+  var $body           = $("body")
+  $videoContainer.prepend("<h1 id='radio-title'>Youtube Radio <a href='#' id='raido-toggle'>disable</a></h1>")
+
+  $body.addClass("dismiss-youtube-radio")
+
+
+  $("#raido-toggle").click(function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    toggleVideo($(this))
+  })
+
+  var toggleVideo = function($title) {
+    $body.toggleClass("dismiss-youtube-radio")
+    if($body.is(".dismiss-youtube-radio")) {
+        $title.html("YR enable")
+        showVideo()
+    } else {
+      hideVideo()
+      $title.html("disable")
+    }
+  }
+
+  var showVideo = function() {
+    var currentHeight = parseInt($('video').css('height'));
+    var newHeight     = currentHeight + 36; // the height of the control bar
+
+    // #player-api is the video master-wrapper
+    $('#player-api').attr('style', 'height:' + newHeight + 'px !important;');
+    // #player-mole-container is the element that pushes the rest of the content down
+    $("#player-mole-container").css('height', (newHeight - newHeight*0.24) + "px" );
+  }
+
+  var hideVideo = function() {
+    $('#player-api').attr('style', 'height: 90px !important;')
+    $("#player-mole-container").css('height', 0);
+  }
+
+  toggleVideo($(this))
+})();
